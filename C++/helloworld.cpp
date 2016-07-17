94bb6c57f698236eb637ddddb76a44b9fd265dac
rocklotus <15337148752@189.cn>
2016-07-17 18:45:32 +0800

    helloworld/C++

---
+++ C++/helloworld.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	# File Name: helloworld.cpp
+	# Author: 
+	# Mail: 
+	# Created Time: 2016年07月15日 星期五 16时08分03秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(void){
+    cout<<"Hello world!\n";
+    return 0;
+}
+
